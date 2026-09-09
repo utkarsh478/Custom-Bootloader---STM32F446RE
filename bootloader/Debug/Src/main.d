ac218ac6fbd82f5fb44c4aa5920ecbd4e0eb230d
Utkarsh <155317360+utkarsh478@users.noreply.github.com>
2026-09-09 20:25:34 +0530

    Add files via upload

---
+++ bootloader/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/stm32f446xx.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/core_cm4.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_version.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_compiler.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_gcc.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/mpu_armv7.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/system_stm32f4xx.h
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/stm32f446xx.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/core_cm4.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_version.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_compiler.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_gcc.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/mpu_armv7.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/system_stm32f4xx.h:
---
+++ bootloader/Debug/Src/system_stm32f4xx.d
@@ -0,0 +1,17 @@
+Src/system_stm32f4xx.o: ../Src/system_stm32f4xx.c \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/stm32f4xx.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/stm32f446xx.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/core_cm4.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_version.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_compiler.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_gcc.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/mpu_armv7.h \
+ E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/system_stm32f4xx.h
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/stm32f4xx.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/stm32f446xx.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/core_cm4.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_version.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_compiler.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/cmsis_gcc.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Include/mpu_armv7.h:
+E:/UT/Emb/workspace/MYWORLD/bootloader/CMSIS/Device/system_stm32f4xx.h:
